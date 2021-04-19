da4bc469f43d3d30d8a5ddcac02ea376061a7ee9
Radu Alexandru <alexx15raduu@gmail.com>
2021-04-19 20:33:25 +0300

    Implemented ZodiacService.

--- ZodiacServer/ZodiacServer/Constants/Constants.cs
+++ ZodiacServer/ZodiacServer/Constants/Constants.cs
@@ -8,5 +8,6 @@ namespace ZodiacServer.Constants
     public class Constants
     {
         public const string ZODIAC_PATH = "./Resources/zodiac.txt";
+        public const string CHANNEL_ADDRESS = "https://localhost:5001";
     }
 }
--- ZodiacServer/ZodiacServer/Services/ZodiacService.cs
+++ ZodiacServer/ZodiacServer/Services/ZodiacService.cs
@@ -1,11 +1,73 @@
-﻿using System;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ZodiacServer.Helper;
 
 namespace ZodiacServer
 {
     public class ZodiacService : ZodiacSign.ZodiacSignBase
     {
+        private Operations operations;
+        public override Task<GetZodiacSignResponse> GetZodiacSign(GetZodiacSignRequest request, ServerCallContext context)
+        {
+            operations = new Operations();
+            using var channel = GrpcChannel.ForAddress(Constants.Constants.CHANNEL_ADDRESS);
+            var date = request.Date;
+            string sign = "";
+            var season = operations.GetSeason(date);
+            if (season.Equals("INVALID"))
+            {
+                Console.WriteLine("INVALID DATE");
+                return Task.FromResult(new GetZodiacSignResponse
+                {
+                    Sign = "INVALID"
+                });
+            }
+
+            switch (season)
+            {
+                case "Spring":
+                    var springClient = new SpringSign.SpringSignClient(channel);
+                    var springResponse = springClient.GetSpringSign(new GetSpringSignRequest
+                    {
+                        Date = date
+                    });
+                    sign = springResponse.Sign;
+                    break;
+                case "Summer":
+                    var summerClient = new SummerSign.SummerSignClient(channel);
+                    var summerResponse = summerClient.GetSummerSign(new GetSummerSignRequest
+                    {
+                        Date = date
+                    });
+                    sign = summerResponse.Sign;
+                    break;
+                case "Autumn":
+                    var autumnClient = new AutumnSign.AutumnSignClient(channel);
+                    var autumnResponse = autumnClient.GetAutumnSign(new GetAutumnSignRequest
+                    {
+                        Date = date
+                    });
+                    sign = autumnResponse.Sign;
+                    break;
+                case "Winter":
+                    var winterClient = new WinterSign.WinterSignClient(channel);
+                    var winterResponse = winterClient.GetWinterSign(new GetWinterSignRequest
+                    {
+                        Date = date
+                    });
+                    sign = winterResponse.Sign;
+                    break;
+                default:
+                    break;
+            }
+            return Task.FromResult(new GetZodiacSignResponse
+            {
+                Sign = sign
+            });
+        }
     }
 }
