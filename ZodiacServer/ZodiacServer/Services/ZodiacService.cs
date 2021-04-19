8d43b2ab49d710b74c44b84fa37eaab6c88fe528
Radu Alexandru <alexx15raduu@gmail.com>
2021-04-19 19:23:24 +0300

    Created ZodiacServer project. Implemented zodiac.proto.

---
+++ ZodiacServer/ZodiacServer/Services/ZodiacService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZodiacServer
+{
+    public class ZodiacService : ZodiacSign.ZodiacSignBase
+    {
+    }
+}
