3a3beceebc310b7406fc2568827acb79217efe9d
Radu Alexandru <alexx15raduu@gmail.com>
2021-04-19 19:29:47 +0300

    Added Operations class. Read zodiac signs from file.

---
+++ ZodiacServer/ZodiacServer/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZodiacServer.Constants
+{
+    public class Constants
+    {
+        public const string ZODIAC_PATH = "./Resources/zodiac.txt";
+    }
+}
---
+++ ZodiacServer/ZodiacServer/Helper/Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZodiacServer.Helper
+{
+    public class Operations
+    {
+        public List<Tuple<string, string, string>> GetZodiacSigns()
+        {
+            var signList = new List<Tuple<string, string, string>>();
+
+            try
+            {
+                var streamReader = new StreamReader(Constants.Constants.ZODIAC_PATH);
+                var line = streamReader.ReadLine();
+                while (line != null)
+                {
+                    var elements = line.Split("|");
+                    signList.Add(new Tuple<string, string, string>(
+                        elements[0],
+                        elements[1],
+                        elements[2]));
+                    line = streamReader.ReadLine();
+                }
+                streamReader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            return signList;
+        }
+    }
+}
