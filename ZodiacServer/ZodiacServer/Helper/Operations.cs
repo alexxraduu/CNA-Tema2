1870e8c7ae1ad07447ba789f719c480d75bbed2a
Radu Alexandru <alexx15raduu@gmail.com>
2021-04-19 19:33:08 +0300

    Added GetSign and GetSeason methods in Operations!

--- ZodiacServer/ZodiacServer/Helper/Operations.cs
+++ ZodiacServer/ZodiacServer/Helper/Operations.cs
@@ -33,5 +33,69 @@ namespace ZodiacServer.Helper
             }
             return signList;
         }
+        public string GetSign(string date)
+        {
+            var elem = date.Split("/");
+            var currentDate = new Tuple<int, int>(Int32.Parse(elem[0]), Int32.Parse(elem[1]));
+
+            foreach (var sign in GetZodiacSigns())
+            {
+                var auxBegin = sign.Item2.Split("/");
+                var beginDate = new Tuple<int, int>(Int32.Parse(auxBegin[0]), Int32.Parse(auxBegin[1]));
+                var auxEnd = sign.Item3.Split("/");
+                var endDate = new Tuple<int, int>(Int32.Parse(auxEnd[0]), Int32.Parse(auxEnd[1])); ;
+
+                if ((currentDate.Item1 == beginDate.Item1 && currentDate.Item2 >= beginDate.Item2) || (currentDate.Item1 == endDate.Item1 && currentDate.Item2 <= endDate.Item2))
+                {
+                    return sign.Item1;
+                }
+
+            }
+            return "INVALID";
+        }
+
+        public string GetSeason(string date)
+        {
+            var aux = date.Split("/");
+            try
+            {
+                var month = Int32.Parse(aux[0]);
+                switch (month)
+                {
+                    case 1:
+                        return "Winter";
+                    case 2:
+                        return "Winter";
+                    case 3:
+                        return "Spring";
+                    case 4:
+                        return "Spring";
+                    case 5:
+                        return "Spring";
+                    case 6:
+                        return "Summer";
+                    case 7:
+                        return "Summer";
+                    case 8:
+                        return "Summer";
+                    case 9:
+                        return "Autumn";
+                    case 10:
+                        return "Autumn";
+                    case 11:
+                        return "Autumn";
+                    case 12:
+                        return "Winter";
+                    default:
+                        return "INVALID";
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return "INVALID";
+        }
     }
 }
