baab1a65a9f2f246683285898ff2ac48a07ddc50
Radu Alexandru <alexx15raduu@gmail.com>
2021-04-19 21:02:48 +0300

    Implemented ZodiacClient task.

--- ZodiacClient/ZodiacClient/Program.cs
+++ ZodiacClient/ZodiacClient/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
 
 namespace ZodiacClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string ReadInput()
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter a date (month/day/year): ");
+            var date = Console.ReadLine();
+          
+            return date;
+        }
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ZodiacSign.ZodiacSignClient(channel);
+            Console.WriteLine("Do you want to enter a date? Y/N");
+            var option = Console.ReadLine();
+            while (option.ToLower().Equals("Y".ToLower()))
+            {
+                var reply = await client.GetZodiacSignAsync(
+                                              new GetZodiacSignRequest { Date = ReadInput() });
+                Console.WriteLine("Sign: " + reply.Sign);
+                Console.WriteLine("\nDo you want to enter another date? Y/N");
+                option = Console.ReadLine();
+            }
         }
     }
 }
